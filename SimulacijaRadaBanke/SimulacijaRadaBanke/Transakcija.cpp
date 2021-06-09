8c37767c69bbe9ebe6573101ad197d22e1fded8a
Milan Sazdov <71905169+MilanSazdov@users.noreply.github.com>
2021-06-09 17:54:28 +0200

    Update 7 - Transakcija

--- SimulacijaRadaBanke/SimulacijaRadaBanke/Transakcija.cpp
+++ SimulacijaRadaBanke/SimulacijaRadaBanke/Transakcija.cpp
@@ -7,6 +7,7 @@ int Transakcija::ID_transakcije = -1;
 
 Transakcija::Transakcija() {
 
+    ID_transakcije++;
     tip_valute = RSD;
     kolicina_novca = 0;
     broj_racuna_posiljalac = 1;
@@ -19,11 +20,12 @@ Transakcija::Transakcija() {
 
 Transakcija::Transakcija(TipValute tip_valute_, double kolicina_novca_, int broj_racuna_primalac_, int broj_racuna_posiljalac_, Datum datum_transakcije_, std::string Primalac_, std::string Posiljalac_, double provizija_) {
 
+    ID_transakcije++;
     tip_valute = tip_valute_;
     kolicina_novca = kolicina_novca_;
     broj_racuna_posiljalac = broj_racuna_posiljalac_;
     broj_racuna_primalac = broj_racuna_primalac_;
-    datum_transakcije = datum_transakcije_;
+    datum_transakcije = Datum(datum_transakcije_);
     Primalac = Primalac_;
     Posiljalac = Posiljalac_;
     provizija = provizija_;
@@ -32,12 +34,64 @@ Transakcija::Transakcija(TipValute tip_valute_, double kolicina_novca_, int broj
 
 Transakcija::Transakcija(const Transakcija& transakcija) {
 
+    ID_transakcije++;
     tip_valute = transakcija.tip_valute;
     kolicina_novca = transakcija.kolicina_novca;
     broj_racuna_posiljalac = transakcija.broj_racuna_posiljalac;
     broj_racuna_primalac = transakcija.broj_racuna_primalac;
-    datum_transakcije = transakcija.datum_transakcije;
+    datum_transakcije = Datum(transakcija.datum_transakcije);
     Primalac = transakcija.Primalac;
     Posiljalac = transakcija.Posiljalac;
     provizija = transakcija.provizija;
+}
+
+
+TipValute Transakcija::getTipValute()const {
+    return tip_valute;
+}
+double Transakcija::getKolicinaNovca()const {
+    return kolicina_novca;
+}
+int Transakcija::getBrojRacunaPosiljalac()const {
+    return broj_racuna_posiljalac;
+}
+int Transakcija::getBrojRacunaPrimalac()const {
+    return broj_racuna_primalac;
+}
+Datum Transakcija::getDatumTransakcija()const {
+    return datum_transakcije;
+}
+std::string Transakcija::getPrimalac()const {
+    return Primalac;
+}
+std::string Transakcija::getPosiljalac()const {
+    return Posiljalac;
+}
+double Transakcija::getProvizija()const {
+    return provizija;
+}
+
+void Transakcija::setTipValute(TipValute tip_valute_) {
+    tip_valute = tip_valute_;
+}
+void Transakcija::setKolicinaNovca(double kolicina_novca_) {
+    kolicina_novca = kolicina_novca_;
+}
+void Transakcija::setBrojRacunaPosiljalac(int broj) {
+    broj_racuna_posiljalac = broj;
+}
+void Transakcija::setBrojRacunaPrimalac(int broj) {
+    broj_racuna_primalac = broj;
+}
+void Transakcija::setDatumTransakcije(Datum datum_) {
+    datum_transakcije = Datum(datum_);
+}
+void Transakcija::setPrimalac(std::string primalac) {
+    Primalac = primalac;
+}
+void Transakcija::setPosiljalac(std::string posiljalac) {
+    Posiljalac = posiljalac;
+}
+void Transakcija::setProvizija(double provizija_) {
+    provizija = provizija_;
 }
--- SimulacijaRadaBanke/SimulacijaRadaBanke/Transakcija.h
+++ SimulacijaRadaBanke/SimulacijaRadaBanke/Transakcija.h
@@ -21,6 +21,27 @@ private:
 
 public:
     static int ID_transakcije;
+
+    //GETERI
+    TipValute getTipValute()const;
+    double getKolicinaNovca()const;
+    int getBrojRacunaPosiljalac()const;
+    int getBrojRacunaPrimalac()const;
+    Datum getDatumTransakcija()const;
+    std::string getPrimalac()const;
+    std::string getPosiljalac()const;
+    double getProvizija()const;
+
+    //SETERI
+
+    void setTipValute(TipValute tip_valute_);
+    void setKolicinaNovca(double kolicina_novca_);
+    void setBrojRacunaPosiljalac(int broj);
+    void setBrojRacunaPrimalac(int broj);
+    void setDatumTransakcije(Datum datum_);
+    void setPrimalac(std::string primalac);
+    void setPosiljalac(std::string posiljalac);
+    void setProvizija(double provizija_);
     
     Transakcija();
     Transakcija(TipValute tip_valute_, double kolicina_novca_, int broj_racuna_primalac_, int broj_racuna_posiljalac, Datum datum_transakcije_, std::string Primalac_, std::string Posiljalac_, double provizija_);
