0efb9dbe568c62649a6d0278cdb6b81a2881ce7e
Milan Sazdov <71905169+MilanSazdov@users.noreply.github.com>
2021-06-09 18:04:21 +0200

    Update Kredit.h - 7

--- SimulacijaRadaBanke/SimulacijaRadaBanke/Krediti.h
+++ SimulacijaRadaBanke/SimulacijaRadaBanke/Krediti.h
@@ -11,15 +11,11 @@ class Kredit {
 
 private:
 
-    // U kredit stavljam id korisnika koji ima kredit, id ziranta;
-    int id_ziranta;
     TipKredita tip_kredita;
     double iznos_kredita;
     Datum rok_isplate;
     double mesecna_rata;
-    bool kamata;
-    double kamata_;
-    bool zirant;
+    double kamata;
     bool placena_rata_za_mesec;
     double placeno_do_sada;
 
@@ -27,8 +23,34 @@ private:
 public:
 
     static int id;
+
+    //Geteri 
+    TipKredita getTipKredita()const;
+    double getIznosKredita()const;
+    Datum getRokIsplate()const;
+    double getMesecnaRata()const;
+    double getKamata()const;
+    bool getPlacenaRataZaMesec()const;
+    double getPlacenoDoSada()const;
+
+    //Seteri
+
+    void setTipKredita(TipKredita tip_kredita_);
+    void setIznosKredita(double iznos_kredita_);
+    void setRokIsplate(Datum rok_isplate_);
+    void setMesecnaRata(double mesecna_rata_);
+    void setKamata(double kamata_);
+    void setPlacenaRataZaMesec(bool placena_rata_za_mesec_);
+    void setPlacenoDoSada(double placeno_do_sada_);
+
     void PlatiRatu(DinarskiRacun& racun);
     void IsplatiCeoKredit(DinarskiRacun& racun);
+
     friend std::ostream& operator<<(std::ostream& os, const Kredit& kredit);
+
+};
+
+class ZahtevZaKredit{
+
 
 };
